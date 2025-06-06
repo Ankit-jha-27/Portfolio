d08a038f81a109ea26d5a0c3fe3644ec06707218
Ankit Jha <jhaankitjha03@gmail.com>
2025-06-07 01:15:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+var typed = new Typed('#element', {
+  strings: ['Student', 'Web Developer','IT Enthusiast'],
+  typeSpeed: 100,
+  loop:true
+});
+
+
+
+var typed = new Typed('#elem', {
+strings: ['Thank you','For Visiting'],
+typeSpeed: 100,
+loop:true
+});
